8aad9d9c909e265f7982f3c14bbd99f7b18e48d4
Yusta21 <noelyusta@hotmail.com>
2024-08-07 18:51:21 +0200

    Initial Commit

---
+++ Rick&MortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&MortyApp
+//
+//  Created by Noel H. Yusta on 7/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
